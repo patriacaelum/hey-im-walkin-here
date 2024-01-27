e3b5c483e76fead0330a244021d25f493124f35d
Austin Nhung <austinnhung@yahoo.ca>
2024-01-26 22:20:32 -0700

    Level automatically spawns cars

--- src/scripts/nyc.gd
+++ src/scripts/nyc.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
 
+var CarScene: PackedScene = preload("res://scenes/car.tscn")
+
+
 func _ready() -> void:
 	$UI.started.connect(self._on_ui_started)
+	$CarSpawnTimer.timeout.connect(self._on_car_spawn_timer_timeout)
 	# Spawn penguin
 	pass
 
 
 func _on_ui_started() -> void:
 	# Start penguin walking
-	# Start car spawning
-	pass
+	$CarSpawnTimer.start(2)
 
 
 func _on_crash_penguin(car) -> void:
@@ -25,3 +28,11 @@ func _on_crash_penguin(car) -> void:
 func _on_ui_upgrade_purchased(upgrade) -> void:
 	# Set upgrade on penguin
 	pass
+
+
+func _on_car_spawn_timer_timeout() -> void:
+	var car: Car = CarScene.instantiate()
+	car.speed = 150
+	car.position = Vector2(-car.POS_X_BUFFER, 256 + 128 * randi_range(0, 10))
+	self.add_child(car)
+	$CarSpawnTimer.start(0.5)
