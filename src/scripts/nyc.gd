a148b7324c5a0184605e2d6a026a9d6b1e972e40
Austin Nhung <austinnhung@yahoo.ca>
2024-01-26 19:17:34 -0700

    Add main level and some pseudocode

---
+++ src/scripts/nyc.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+func _ready() -> void:
+	# Spawn penguin
+	# Show UI
+	pass
+
+
+func _on_start_ui() -> void:
+	# Start penguin walking
+	# Start car spawning
+	pass
+
+
+func _on_crash_penguin(car) -> void:
+	# Destroy armour on penguin
+	# if penguin has no more armour:
+	#     end current launch and show UI
+	# Update car state to crash
+	pass
