23300e8b080f90659c1bcbac1804b01cead13df6
Austin Nhung <austinnhung@yahoo.ca>
2024-01-26 22:20:17 -0700

    Move car scene and make it despawn when out of bounds

---
+++ src/scripts/car.gd
@@ -0,0 +1,36 @@
+class_name Car
+extends Area2D
+
+
+signal crashed
+
+
+@export var speed: int = 0
+
+var pos: Vector2
+var broken: bool = false
+
+const POS_X_BUFFER: int = 256
+
+
+func _ready() -> void:
+	self.body_entered.connect(self._on_body_entered)
+
+
+func _process(delta: float) -> void:
+	self.position.x += speed * delta
+
+	if self.__out_of_bounds():
+		self.queue_free()
+
+
+func _on_body_entered(_body):
+	self.broken = true
+	self.crashed.emit()
+
+
+func __out_of_bounds() -> bool:
+	var left_boundary: bool = self.position.x < -self.POS_X_BUFFER
+	var right_boundary: bool = self.position.x > self.get_viewport_rect().size.x + self.POS_X_BUFFER
+
+	return left_boundary or right_boundary
